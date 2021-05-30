fb5874181b680534e8c56229fc34c70161d6016d
Shivam Riyar <55154187+Shivam-Riyar@users.noreply.github.com>
2021-05-30 22:38:56 +0530

    Update q1.py

--- PRACTICE PROBLEMS/CLASS 3/UNSOLVED/q1.py
+++ PRACTICE PROBLEMS/CLASS 3/UNSOLVED/q1.py
@@ -1,1 +1,17 @@
-# TAKE TWO 
+# TAKE TWO NUMBERS AS INPUT AND PERFORM ALL THE ARITHMETIC OPERATIONS IN IT
+x = int(input("ENTER FIRST NUMBER : "))
+y = int(input("ENTER SECOND NUMBER : "))
+add = x + y
+sub = x - y
+mult = x * y
+div = x / y
+floordiv = x//y
+mod = x % y
+exp = x ** y
+print("SUM : ",add)
+print("DIFFERENCE : ",sub)
+print("MULTIPLICATION : ",mult)
+print("DIVISION : ",div)
+print("FLOOR DIVISION : ",floordiv)
+print("MODULUS : ",mod)
+print("EXPONENT : ",exp)
